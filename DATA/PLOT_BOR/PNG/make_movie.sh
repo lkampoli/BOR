6ed7c96e710c70c5a9028da98e44697a5e8ee438
Lorenzo <lorenzo.campoli@unimelb.edu.au>
2023-05-10 13:42:15 +1000

    update BOR

--- DATA/PLOT_BOR/PNG/make_movie.sh
+++
@@ -1,2 +0,0 @@
-#ffmpeg -framerate 10 -pattern_type glob -i "*.png"
-ffmpeg -framerate 10 -pattern_type glob -i "c[012345]-*.png" out.swf
--- DATA/PLOT_BOR/make_movie_lines.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -framerate 5 -pattern_type glob -i "c[012345]-*.png" cluster_profiles.mp4
